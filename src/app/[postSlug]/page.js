f213df4e9b35a9ae23f258fed0657d1c233de16d
zsigmund-od <zsigmund@overdrive.com>
2026-04-22 15:14:09 -0400

    Ex3.2: Dynamic post metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 import styles from "./postSlug.module.css";
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+  const { frontmatter } = await loadBlogPost(postSlug);
+
+  return {
+    title: frontmatter.title,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
