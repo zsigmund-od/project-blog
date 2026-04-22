fb0374823aa3ceb3f0a74ece1b42a6883b0b7414
zsigmund-od <zsigmund@overdrive.com>
2026-04-22 13:15:01 -0400

    Ex2.1: Load post file and render metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
+import { loadBlogPost } from "@/helpers/file-helpers";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
 
-function BlogPost() {
+async function BlogPost({ params }) {
+  const { postSlug } = await params;
+  const { frontmatter, content } = await loadBlogPost(postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={new Date(frontmatter.publishedOn)}
       />
       <div className={styles.page}>
         <p>This is where the blog post will go!</p>
         <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
+          You will need to use <em>MDX</em> to render all of the elements
+          created from the blog post in this spot.
         </p>
       </div>
     </article>
