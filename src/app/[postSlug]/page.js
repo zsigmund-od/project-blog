48c3033fdb50a20031364290a25d7d90b8921da1
zsigmund-od <zsigmund@overdrive.com>
2026-04-22 15:33:52 -0400

    Ex4: Render pre as CodeSnippet

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
+import CodeSnippet from "@/components/CodeSnippet";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 import styles from "./postSlug.module.css";
@@ -16,6 +17,12 @@ export async function generateMetadata({ params }) {
   };
 }
 
+const components = {
+  pre: (props) => (
+    <CodeSnippet {...props} />
+  )
+}
+
 async function BlogPost({ params }) {
   const { postSlug } = await params;
   const { frontmatter, content } = await loadBlogPost(postSlug);
@@ -27,7 +34,7 @@ async function BlogPost({ params }) {
         publishedOn={new Date(frontmatter.publishedOn)}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={components} />
       </div>
     </article>
   );
